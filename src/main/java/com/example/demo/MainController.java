6c2d49d9ba04097af8f27e2759e4d7fe069697d0
HimanshuYadaavv <yhimanshu71@gmail.com>
2023-06-23 10:14:55 +0530

    Login and SignUp

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,8 @@ import java.util.Optional;
 public class MainController {
     @Autowired
     CredentialRepository credentialRepository;
+    @Autowired
+    private UserdetailRepository userdetailRepository;
 
     @GetMapping("/Land")
     public String LandingPage(){
@@ -28,12 +31,35 @@ public class MainController {
         credentialRepository.save(credential);
         return "dashboard";
     }
+
+    public String username(@RequestParam("username") String username,
+                           @RequestParam("firstname") String firstname,
+                           @RequestParam("lastname") String lastname,
+                           @RequestParam("email") String email,
+                           @RequestParam("phone") String phone, HttpSession session){
+
+        Userdetail userdetail=new Userdetail();
+        userdetail.setUsername(username);
+        userdetail.setFirstname(firstname);
+        userdetail.setLastname((lastname));
+        userdetail.setEmail(email);
+        userdetail.setPhone(phone);
+        return "dashboard" ;
+    }
+
+
+
+
     @PostMapping("/login")
-    public  String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session){
+    public  String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session , Model model){
         Optional<Credential> matchCredential= credentialRepository.findById(username);
         if (matchCredential.isPresent()){
             if (matchCredential.get().getPassword().equals(password))
-            {
+            {   session.setAttribute("username", username);
+                Optional<Userdetail> userdetail= userdetailRepository.findById(username);
+                if(userdetail.isPresent()){
+                    model.addAttribute("userdetail",userdetail.get());
+                }
                 return "dashboard";
             }else
             {
@@ -44,7 +70,6 @@ public class MainController {
 
             return "Landingpage";
         }
-
 
 
     }
