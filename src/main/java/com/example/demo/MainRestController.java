8ad29bba2c3fb0fa3aa925d1b2766c2f7229c332
HimanshuYadaavv <yhimanshu71@gmail.com>
2023-06-29 20:01:50 +0530

    Login and SignUp

--- src/main/java/com/example/demo/MainRestController.java
+++ src/main/java/com/example/demo/MainRestController.java
@@ -1,19 +1,33 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/0.1")
+@CrossOrigin
 public class MainRestController {
+
+
 
 @Autowired
 UsertypelinkRepository usertypelinkRepository;
+    @Autowired
+    private UserdetailRepository userdetailRepository;
+
+
+    @GetMapping("/newuserdetail")
+    public Userdetail getDummyUser(){
+    Userdetail userdetail=new Userdetail();
+    userdetail.setUsername("username");
+    userdetail.setFirstname("firstname");
+    userdetail.setLastname("lastname");
+    userdetail.setEmail("email");
+    userdetail.setPhone("phone");
+    return userdetail;
+}
 
 @GetMapping("/getusertypes")
 public List<Usertypelink> getUserType(@RequestParam("username") String username){
