318a4f1f5fba08598a6a76fecbb2eacfa64ae3c1
HimanshuYadaavv <yhimanshu71@gmail.com>
2023-06-29 20:18:14 +0530

    Login and SignUp

---
+++ src/main/java/com/example/MainRestController.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/0.1")
+@CrossOrigin
+public class MainRestController {
+
+
+
+@Autowired
+UsertypelinkRepository usertypelinkRepository;
+    @Autowired
+    private UserdetailRepository userdetailRepository;
+
+
+    @GetMapping("/newuserdetail")
+    public Userdetail getDummyUser(){
+    Userdetail userdetail=new Userdetail();
+    userdetail.setUsername("username");
+    userdetail.setFirstname("firstname");
+    userdetail.setLastname("lastname");
+    userdetail.setEmail("email");
+    userdetail.setPhone("phone");
+    return userdetail;
+}
+
+@GetMapping("/getusertypes")
+public List<Usertypelink> getUserType(@RequestParam("username") String username){
+
+    return usertypelinkRepository.findbyUsername(username);
+    }
+
+
+
+
+
+
+}
